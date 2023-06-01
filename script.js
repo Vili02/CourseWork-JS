ecaab7ec8ae056e2dbee0a51dd76ac2d0de2741a
Velislava <viliasparuhova02@gmail.com>
2023-06-01 19:59:59 +0300

    fix and update script

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-const addTrailingZero = (num) => {
-    return num < 10 ? "0" + num : num;
-};
-
-const updateTime = () => {
+ //Clock
+const updateTime = () =>{
     const time = new Date();
     let hours = time.getHours();
     let minutes = time.getMinutes();
@@ -30,6 +27,33 @@ const updateTime = () => {
     //call function after every second
     setInterval(updateTime,1000);
 
+    $(".stopwatch-btn").click(function (){
+        //hide all other wrappers
+        $(".main-container > div").slideUp();
+        //show stopwatch wrapper
+        $(".stopwatch").slideDown();
+        //update type text
+        $(".type").html("Stopwatch"); 
+    });
+    
+    $(".timer-btn").click(function (){
+        //hide all other wrappers
+        $(".main-container > div").slideUp();
+        //show timer wrapper
+        $(".timer").slideDown();
+        //update type text
+        $(".type").html("Timer"); 
+    });
+
+    $(".back-btn").click(function (){
+        //hide all other wrappers
+        $(".main-container > div").slideUp();
+        //show clock wrapper
+        $(".clock").slideDown();
+        //update type text
+        $(".type").html("Clock"); 
+    });
+
     //Stopwatch
     let stopwatchHours = 0,
         stopwatchMinutes = 0,
@@ -39,10 +63,10 @@ const updateTime = () => {
         laps = 0,
         stopwatchInterval;
 
-        const stopwatch = () =>{
+        function stopwatch () {
             //incerase miliseconds by one
             stopwatchMiliSeconds++;
-
+            
             if(stopwatchMiliSeconds === 100){
                 stopwatchSeconds++;
                 stopwatchMiliSeconds = 0;
@@ -62,38 +86,37 @@ const updateTime = () => {
             $("#stopwatch-min").html(addTrailingZero(stopwatchMinutes));
             $("#stopwatch-sec").html(addTrailingZero(stopwatchSeconds));
             $("#stopwatch-ms").html(addTrailingZero(stopwatchMiliSeconds));
-        };
+        }
 
-        const startStopwatch = () =>{
+        function startStopwatch () {
             if(!stopwatchRunning){
                 stopwatchInterval = setInterval(stopwatch,10);
                 stopwatchRunning = true;
             }
-        };
+        }
+
     //function to stop stopwatch
-        const stopStopwatch = () =>{
+        function stopStopwatch () {
             clearInterval(stopwatchInterval);
             stopwatchRunning = false;
-        };
+        }
 
-    //reset stopwatch function
-        const resetStopwatch = () => {
+        //reset stopwatch function
+        function resetStopwatch () {
         //clear interval and set all values to default
             clearInterval(stopwatchInterval);
-            stopwatchHours = 0,
-            stopwatchMinutes = 0,
-            stopwatchSeconds = 0,
-            stopwatchMiliSeconds = 0,
-            stopwatchRunning = false,
-            laps = 0,
+            stopwatchHours = 0;
+            stopwatchMinutes = 0;
+            stopwatchSeconds = 0;
+            stopwatchMiliSeconds = 0;
+            laps = 0;
 
         //update values on document to 00
             $("#stopwatch-hour").html("00");
             $("#stopwatch-min").html("00");
             $("#stopwatch-sec").html("00");
             $("#stopwatch-ms").html("00");
-            $(".laps").html("");
-        };
+        }
 
     //start stopwatch on start button
         $(".start-stopwatch").click(function (){
@@ -107,6 +130,7 @@ const updateTime = () => {
             resetStopwatch();
             $(".start-stopwatch").show();
             $(".lap-stopwatch").hide();
+            $(".laps").html("");
         });
 
         $(".lap-stopwatch").click(function (){
@@ -127,36 +151,41 @@ const updateTime = () => {
           );
         }); 
 
-        let time = 0;
+        function addTrailingZero (number) {
+            return number < 10 ? "0" + number : number;
+        }
+
+        let time = 0,
         timerHours = 0,
         timerMinutes = 0,
         timerSeconds = 0,
         timerMiliSeconds = 0,
+        timerRunning = false,
         timerInterval;
 
-        const getTime = () =>{
+        function getTime () {
             time = prompt("Enter time in minutes");
             //convert time to seconds
             time = time * 60;
             //update timer defaults
             setTime();
-        };
+        }
 
-        const setTime = () =>{
+        function setTime () {
             timerHours = Math.floor(time / 3600);
-            timerMinutes = Math.floor((time / 3600) / 60);
+            timerMinutes = Math.floor((time % 3600) / 60);
             timerSeconds = Math.floor(time % 60);
 
             $("#timer-hour").html(addTrailingZero(timerHours));
             $("#timer-min").html(addTrailingZero(timerMinutes));
             $("#timer-sec").html(addTrailingZero(timerSeconds));
             $("#timer-ms").html(addTrailingZero(timerMiliSeconds));
-        };
+        }
 
-        const timer = () =>{
-            imerMiliseconds--;
-              if (timerMiliseconds === -1) {
-                timerMiliseconds = 99;
+        function timer() {
+            timerMiliSeconds--;
+              if (timerMiliSeconds === -1) {
+                timerMiliSeconds = 99;
                 timerSeconds--;
               }
               if (timerSeconds === -1) {
@@ -167,10 +196,63 @@ const updateTime = () => {
                 timerMinutes = 59;
                 timerHours--;
               }
+                $("#timer-hour").html(addTrailingZero(timerHours));
+                $("#timer-min").html(addTrailingZero(timerMinutes));
+                $("#timer-sec").html(addTrailingZero(timerSeconds));
+                $("#timer-ms").html(addTrailingZero(timerMiliSeconds));
+
+            //check time up on every interval
+            timeUp();
+        };
+
+        function startTimer() {
+            //before start check if valid time given
+            if(timerHours === 0 && timerMinutes === 0 && timerSeconds === 0 && timerMiliSeconds === 0){
+                //if all values are zero get time
+                getTime();
+            }else{
+                //start timer
+                timerInterval = setInterval(timer,10);
+                timerRunning = true;
+                $(".start-timer").hide();
+                $(".stop-timer").show();                
+            }
         }
 
-        $(".start-timer").click(function (){
-            getTime();
+        function stopTimer () {
+            clearInterval(timerInterval);
+            timerRunning = false;
+            $(".start-timer").show();
+            $(".stop-timer").hide();
+        }
+
+        function resetTimer () {
+            stopTimer();
+            time = 0;
+            setTime();
+        }
+
+        //check if time remaining
+        function timeUp () {
+            if(timerHours === 0 && 
+                timerMinutes === 0 && 
+                timerSeconds === 0 && 
+                timerMiliSeconds === 0){
+                stopTimer();
+                alert("Time's up!");
+            }
+        }
+
+        $(".start-timer").click(startTimer);
+
+        $(".stop-timer").click(stopTimer);
+
+        $(".reset-timer").click(function(){
+            resetTimer();
+            if (!timerRunning) {
+                $(".start-timer").show();
+                $(".stop-timer").hide();
+              }
         });
 
 
